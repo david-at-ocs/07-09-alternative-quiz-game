08e38d569658bae3b141c3279135382d7dfac63c
David Gardetti <dgardetti@gmail.com>
2015-07-09 17:29:22 -0500

    part 2 working

--- global.js
+++ global.js
@@ -1,8 +1,8 @@
-var question1 = {question: "How many words are in this sentence?", possibleAnswers: "A.1\rB. 55\rC. 345\rD. 7\rE. 4", answer: "D"};
-var question2 = {question: "What about this sentence?", possibleAnswers: "A. 1\rB. 55\rC. 345\rD. 7\rE. 4", answer: "E"};
-var question3 = {question: "And this sentence?", possibleAnswers: "A. 1\rB. 55\rC. 345\rD. 7\rE. 3", answer: "E"};
-var question4 = {question: "What's your favorite letter.", possibleAnswers: "A.\rB.\rC.\rD.\rE.", answer: "B"};
-var question5 = {question: "Is there a correct answer to this question?", possibleAnswers: "A. Yes\rB. No\rC. Not Sure\rD. Maybe", answer: "A"}
+var question1 = {possibleAnswers: "A.1\rB. 55\rC. 345\rD. 7\rE. 4", answer: "D"};
+var question2 = {possibleAnswers: "A. 1\rB. 55\rC. 345\rD. 7\rE. 4", answer: "E"};
+var question3 = {possibleAnswers: "A. 1\rB. 55\rC. 345\rD. 7\rE. 3", answer: "E"};
+var question4 = {possibleAnswers: "A.\rB.\rC.\rD.\rE.", answer: "B"};
+var question5 = {possibleAnswers: "A. Yes\rB. No\rC. Not Sure\rD. Maybe", answer: "A"}
 var score = 0;
 var count = 0;
 var quest;
@@ -41,13 +41,9 @@ function update_question_result(correct) {
   }
 }
 
-// function process_answer_submission() {
-//   var user_answer = given_answer();
-//   update_question_result(is_correct_answer(user_answer));
-// }
 
 function next_question() {
-  count++
+  ++count
   if (questionsArray[count]) {
     clear_fields();
     set_next_question(count);
@@ -59,12 +55,12 @@ function next_question() {
 function clear_fields() {
   document.getElementById("answer").value = null;
   document.getElementById("question_result").innerText = "";
-  document.getElementById("question").innerText = "";
+  document.getElementById("question" + count).classList.remove("unhide");
   document.getElementById("choices").innerText = "";
 }
 
 function set_next_question(count) {
-  quest.innerText = questionsArray[count].question;
+  document.getElementById("question" + (count+1)).classList.add("unhide");
   choice.innerText = questionsArray[count].possibleAnswers;
 }
 
